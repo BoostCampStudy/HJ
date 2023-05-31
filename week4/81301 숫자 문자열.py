bb9dc9c603265740d387a78f6bfb3c3f7809d96f
HyeonJeong <wgwjh05169@gmail.com>
2023-05-31 23:07:23 +0900

    Update

--- week4/81301 숫자 문자열.py
+++ week4/81301 숫자 문자열.py
@@ -1,23 +1,15 @@
 # result
 #   correctness: 100.0
-#   runtime: 0.03 ~ 0.07ms
-# Cons
-#   dictionary 제대로 활용하지 못한 느낌
+#   runtime: 0.02 ~ 0.04ms
+# key
+#   dictionary 와 replace를 활용하여 반복 시간 줄이기
 
 
 def solution(s):
     stoi = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6' \
         , 'seven': '7', 'eight': '8', 'nine': '9', 'zero': '0'}
-    answer = ''
 
-    history = ''
-    for c in s:
-        if c in stoi.values():
-            answer += c
-        else:
-            history += c
-            if history in stoi.keys():
-                answer += stoi[history]
-                history = ''
+    for k, v in stoi.items():
+        s = s.replace(k, v)
 
-    return int(answer)
+    return int(s)
