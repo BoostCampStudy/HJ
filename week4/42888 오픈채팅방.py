d906bea158362f8a23d9bd385e215ee947fe92e8
HyeonJeong <wgwjh05169@gmail.com>
2023-06-04 23:51:52 +0900

    Create

--- week4/42888 오픈채팅방.py
+++ week4/42888 오픈채팅방.py
@@ -1,9 +1,11 @@
 # result:
 #     correctness: 100.0
-#     runtime: 0.01~104.36ms
+#     runtime: 0.01~123.54ms
 # key:
 #     순서에 따른 유저 맵핑, 유저에 따른 닉네임 맵핑
 #     유저별로 동작을 기록한 후, 마지막에 유저에 따라 닉네임을 추가함
+# key!
+#     항상 닉네임을 추가하거나 삭제하는 경우만 있어서, 존재하는지 검사할 필요가 없음
 
 def solution(record):
     mapping = []  # index -> uid
@@ -17,14 +19,10 @@ def solution(record):
             mapping.append(uid)
         else:
             uid, name = uid.split()
-            if not user.get(uid) or user.get(uid) != name:
-                user[uid] = name
-
+            user[uid] = name
             if action == 'Enter':
                 answer.append("님이 들어왔습니다.")
                 mapping.append(uid)
-            else:
-                user[uid] = name
 
     for i in range(len(mapping)):
         answer[i] = user[mapping[i]] + answer[i]
